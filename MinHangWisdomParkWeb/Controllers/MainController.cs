f37cf71005be6204f72f8d7cffc66e4cd41a29b7
anjijsb <luchen@anjismart.com>
2015-06-17 09:28:39 +0800

    201506170928

--- MinHangWisdomParkWeb/Controllers/MainController.cs
+++ MinHangWisdomParkWeb/Controllers/MainController.cs
@@ -16,5 +16,42 @@ namespace MinHangWisdomParkWeb.Controllers
             return View();
         }
 
+
+        /// <summary>
+        /// 官网
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Main()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// 登陆
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult login()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 论坛管理
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult LunTan()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 临时入口
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult RuKou()
+        {
+            return View();
+        }
     }
 }
